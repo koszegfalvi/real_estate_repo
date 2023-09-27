fb7439555e860b96ba503f84df79e18c5c72bdcc
koszegfalvi <128134080+koszegfalvi@users.noreply.github.com>
2023-09-27 11:27:46 +0200

    vmi hiba van

--- src/main/java/hu/progmatic/real_estate_repo/RealEstateRepoApplication.java
+++ src/main/java/hu/progmatic/real_estate_repo/RealEstateRepoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RealEstateRepoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RealEstateRepoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateRepoApplication.class, args);
+    }
 
 }
--- src/test/java/hu/progmatic/real_estate_repo/RealEstateRepoApplicationTests.java
+++ src/test/java/hu/progmatic/real_estate_repo/RealEstateRepoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RealEstateRepoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
